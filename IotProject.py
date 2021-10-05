4019222b3d5dfb665c0b9438bc964bf683a4e14f
leonardo0598 <leonardoscandino@gmail.com>
2021-10-05 18:48:13 +0200

    4 luci

--- IotProject.py
+++ IotProject.py
@@ -1,46 +1,65 @@
-#definizione per accendere luci al 50% aggiungendo input e nome luce
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-
-
-#setup luci
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(19, GPIO.OUT)
-
-
-#setup moduli IR
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(27, GPIO.IN)
-
 #GPIO.output(18, GPIO.LOW)
+
 #do i nomi con pwm data e frequenza
+
 pwm = GPIO.PWM(18, 10000)
-pwm1 = GPIO.PWM(12, 10000)
-pwm2 = GPIO.PWM(13, 10000)
-pwm3 = GPIO.PWM(19, 10000)
+
+#pwm1 = GPIO.PWM(12, 10000)
+
+pwm2 = GPIO.PWM(19, 10000)
+
+#pwm3 = GPIO.PWM(19, 10000)
+
+
 
 #liste per eseguire in parallelo la funzione dimmerLuce
-LucePwm = ["pwm", "pwm1", "pwm2", "pwm3"] #lista delle luci da salvare con queste variabili
-IrSensors = [22, 23, 24, 27] #GPIO dei sensori di movimento
+
+#LucePwm = [pwm, pwm1, pwm2, pwm3] #lista delle luci da salvare con queste variabili
+LucePwm = [pwm,pwm2] 
+#IrSensors = [22, 23, 24, 27] #GPIO dei sensori di movimento
+IrSensors = [22, 24]
+
 
 #prende in input luce e sensore IR accende la luce al 50% e appena passa qualcosa la alza al 100%
+
 def dimmerLuce(i, j):  
+
     #count = 0
-    j.start(50)
+    m = j
+    m.start(40)
+
     if(GPIO.input(i) == False):
-        time.sleep(1) #se falso si ferma un secondo per risolvere il problema del sensore
+
+        time.sleep(0.1) #se falso si ferma un secondo per risolvere il problema del sensore
+
         if(GPIO.input(i) == False):
-            j.start(100)
+
+            m.start(100)
+
             time.sleep(3)
 
+
+
 #funzione main per eseguire tutto
-while(True):
-    time.sleep(0.1) #per regolare il ciclo while 
-    for i in LucePwm: #esegue le 5 operazioni in parallelo
-        dimmerLuce(IrSensors[i], LucePwm[i]) #esegue dimmer su tutte le luci
+
+try:
+  
+    while(True):
+
+        #time.sleep(0.1) #per regolare il ciclo while 
     
+        for f in range(0,2) : #esegue le 5 operazioni in parallelo
+
+            dimmerLuce(IrSensors[f], LucePwm[f]) #esegue dimmer su tutte le luci
+
+except KeyboardInterrupt:
+
+    pwm.stop()
+
+    pwm1.stop()
+
+    pwm2.stop()
+
+    pwm3.stop()
+
+    GPIO.cleanup()
---
+++ provaluci.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+
+
+
+global pwm
+global pwm1
+global pwm2
+global pwm3
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, GPIO.LOW)
+GPIO.setup(12, GPIO.OUT)
+GPIO.output(12, GPIO.LOW)
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.LOW)
+GPIO.setup(19, GPIO.OUT)
+GPIO.output(19, GPIO.LOW)
+pwm = GPIO.PWM(18, 10000)
+pwm1 = GPIO.PWM(12, 10000)
+pwm2 = GPIO.PWM(13, 10000)
+pwm3 = GPIO.PWM(19, 10000)
+
+pwm.start(100)
+pwm1.start(100)
+pwm2.start(100)
+pwm3.start(100)
+time.sleep(3)
+pwm.start(50)
+pwm1.start(50)
+pwm2.start(50)
+pwm3.start(50)
+time.sleep(3)
+
+pwm.stop()
+pwm1.stop()
+pwm2.stop()
+pwm3.stop()
+GPIO.cleanup()
